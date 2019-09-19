e4df450b42e5ea254feefaa848bc59b503264ab5
Malin <wilting86@hotmail.com>
2019-09-19 10:43:20 +0200

    hejhej

--- app/etc/config.php
+++ app/etc/config.php
@@ -232,6 +232,7 @@ return [
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
         'Mageplaza_Core' => 1,
+        'Mageplaza_BannerSlider' => 1,
         'Mageplaza_Gdpr' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1
